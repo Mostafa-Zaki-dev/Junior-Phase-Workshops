64c4b0f85bdad4ada68770af543c480a19f2b90f
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-01-26 00:12:15 +0200

    Solved with POST verb and data response to the html form

--- PairExercise.Wizard-news-pt3/routes/posts.js
+++ PairExercise.Wizard-news-pt3/routes/posts.js
@@ -31,4 +31,33 @@ postRouter.get('/:id', async (req, res, next) => {
   }
 });
 
+postRouter.post('/', async (req, res) => {
+  try {
+    const name = req.body.name;
+    const title = req.body.title;
+    const content = req.body.content;
+
+    let userData = await client.query('SELECT * FROM users WHERE users.name = $1', [name]);
+
+    if (!userData.rows.length) {
+      userData = await client.query(`INSERT INTO users (name) VALUES ($1) RETURNING *`, [name]);
+    }
+
+    const userId = userData.rows[0].id;
+    const postData = await client.query(
+      `INSERT INTO posts (userId, title, content) 
+    VALUES ($1, $2, $3) RETURNING *`,
+      [userId, title, content]
+    );
+    const postId = postData.rows[0].id;
+    const upvoteData = await client.query(
+      'INSERT INTO upvotes (userId, postId) VALUES ($1, $2) RETURNING *',
+      [userId, postId]
+    );
+    res.redirect(`/posts/${postId}`); // Redirect to the post details page
+  } catch (err) {
+    res.status(500).send('SOMETHING WENT WRONG', err);
+  }
+});
+
 module.exports = postRouter;
