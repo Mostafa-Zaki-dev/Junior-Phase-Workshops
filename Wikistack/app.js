5b1c8146f4dd1813a668bf7f6fdc5312cf21f60f
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-01-28 23:57:12 +0200

    Added routes

--- Wikistack/app.js
+++ Wikistack/app.js
@@ -3,15 +3,19 @@ const morgan = require('morgan');
 const pg = require('pg');
 const { db, User, Page } = require('./models');
 const layout = require('./views/layout');
+const wikiRouter = require('./routes/wiki');
+const usersRouter = require('./routes/users');
+const { blue, red } = require('chalk');
 
 const app = express();
 db.authenticate().then(() => {
-  console.log('connected to the database');
+  console.log(blue('connected to the database'));
 });
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
+app.use('/wiki', wikiRouter);
 
 app.get('/', (req, res, next) => {
   res.send(layout(' '));
@@ -23,7 +27,7 @@ const init = async () => {
   await User.sync({ force: true });
   await Page.sync({ force: true });
   app.listen(PORT, () => {
-    console.log('App is listening on port 1337 ');
+    console.log(blue('App is listening on port 1337 '));
   });
 };
 init();
