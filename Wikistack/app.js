41559084a6a6b881fcee1340b71dfb602679f819
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-01-28 17:47:55 +0200

    Adding models

--- Wikistack/app.js
+++ Wikistack/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const pg = require('pg');
-const Sequelize = require('sequelize');
+const { db } = require('./models');
+const layout = require('./views/layout');
+
 const app = express();
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.send(layout(' '));
 });
 
 const PORT = 1337;
