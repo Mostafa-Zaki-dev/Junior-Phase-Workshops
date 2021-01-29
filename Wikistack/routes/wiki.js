31fd3878942827ec132d2a4cab2495e11b5fa615
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-01-29 14:27:33 +0200

    Added Slug beforeValidate Hook

--- Wikistack/routes/wiki.js
+++ Wikistack/routes/wiki.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { addPage } = require('../views');
+const { Page } = require('../models');
 
 router.get('/', (req, res, next) => {
   res.send('got to GET /wiki/');
 });
 
-router.post('/', (req, res, next) => {
-  res.send('got to POST /wiki/');
+router.post('/', async (req, res, next) => {
+  try {
+    const page = await Page.create({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/add', (req, res, next) => {
