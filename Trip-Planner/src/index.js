613a6cb4d7e614a7d2507b61fd23d595a78ebda8
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-02-03 16:26:58 +0200

    Map up and running

--- Trip-Planner/src/index.js
+++ Trip-Planner/src/index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibW9zdGFmYS16YWtpIiwiYSI6ImNra3BpMDF6NDJlOHcydnRkZm11Nm4ybTcifQ.efjez4xeMaY8eWXt7SR4iQ';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container id name in index.html file
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
