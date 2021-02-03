728804bcbec50cecefe1d85452db3ab8db12a3e0
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-02-03 23:22:58 +0200

    Solved

--- Trip-Planner/src/index.js
+++ Trip-Planner/src/index.js
@@ -7,5 +7,12 @@ const map = new mapboxgl.Map({
   container: 'map', // container id name in index.html file
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v11', // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement('div'); // Create a new, detached DIV
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/cqR6pUI.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
