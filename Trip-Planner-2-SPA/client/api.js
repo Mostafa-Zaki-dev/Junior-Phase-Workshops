83c635004afcc8c9d3f153526414c2285cbc7f36
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-02-11 03:58:41 +0200

    Adding await to the asynchoronus function response.json()

--- Trip-Planner-2-SPA/client/api.js
+++ Trip-Planner-2-SPA/client/api.js
@@ -2,7 +2,7 @@ const fetchAttractions = async () => {
   try {
     const url = 'http://localhost:3000/api';
     const response = await fetch(url);
-    const data = response.json();
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
