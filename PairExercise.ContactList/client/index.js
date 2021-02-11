892f5bb21e745ff3c04494f198825016ffedd1b9
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-02-11 04:12:47 +0200

    AJAX used in lifecycle

--- PairExercise.ContactList/client/index.js
+++ PairExercise.ContactList/client/index.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ContactList from './ContactList';
-
-const contacts = [
-  { id: 1, name: 'R2-D2', phone: '222-222-2222', email: 'r2d2@droids.com' },
-  { id: 2, name: 'C-3PO', phone: '333-333-3333', email: 'c3po@droids.com' },
-  { id: 3, name: 'BB-8', phone: '888-888-8888', email: 'bb8@droids.com' },
-];
+import axios from 'axios';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts,
+      contacts: [],
     };
+  }
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('/api/contacts');
+      console.log(data);
+      this.setState({ contacts: data });
+    } catch (error) {
+      console.error(error);
+    }
   }
   render() {
     const { contacts } = this.state;
