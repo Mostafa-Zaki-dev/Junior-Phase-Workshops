8193fd8aa41981911f52769805676e1b4c5e335a
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-02-17 06:09:24 +0200

    Created Redux Store

---
+++ PairExercise.ReduxGroceries/client/store.js
@@ -0,0 +1,38 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+// ACTION TYPES:
+
+const ADD_GROCERY = 'ADD_GROCERY';
+
+// ACTION CREATORS:
+
+let nextId = 0;
+
+export const addGrocery = (groceryName) => ({
+  type: ADD_GROCERY,
+  id: nextId++, // shouldn't it be pre-increment ++nextId ?
+  text: groceryName,
+});
+
+// INITIAL STATE:
+
+const initialState = { groceries: [] };
+
+// REDUCER:
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_GROCERY:
+      let newGroceryItem = { id: action.id, text: action.text, bought: false };
+      return { ...state, groceries: [...state.groceries, newGroceryItem] };
+    default:
+      return newState;
+  }
+};
+
+// STORE:
+
+const store = createStore(reducer, applyMiddleware(logger));
+
+export default store;
