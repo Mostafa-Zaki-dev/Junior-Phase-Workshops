e47668d9daaabb0554dc2baa522673d5cb7edf68
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-03-02 20:51:35 +0200

    Added Single Story Route

--- PairExercise.Readium-with-Redux/client/components/Main.js
+++ PairExercise.Readium-with-Redux/client/components/Main.js
@@ -1,33 +1,37 @@
-import React from 'react'
-import Navbar from './Navbar'
-import StoriesList from './StoriesList'
-import {connect} from 'react-redux'
-import {fetchStories} from '../store/stories'
+import React from 'react';
+import Navbar from './Navbar';
+import StoriesList from './StoriesList';
+import { connect } from 'react-redux';
+import { fetchStories } from '../store/stories';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 class Main extends React.Component {
   componentDidMount() {
-    this.props.loadStories()
+    this.props.loadStories();
   }
 
-  render () {
+  render() {
     return (
-      <div id='main'>
-        <div className='column container'>
-          <div id='header'>
-            <h1>Readium</h1>
+      <Router>
+        <div id="main">
+          <div className="column container">
+            <div id="header">
+              <h1>Readium</h1>
+            </div>
+            <Navbar />
           </div>
-          <Navbar />
+          <Route path="/stories" component={StoriesList} />
+          <Route exact path="/" component={StoriesList} />
         </div>
-        <StoriesList />
-      </div>
-    )
+      </Router>
+    );
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    loadStories: () => dispatch(fetchStories())
-  }
-}
+    loadStories: () => dispatch(fetchStories()),
+  };
+};
 
-export default connect(null, mapDispatchToProps)(Main)
+export default connect(null, mapDispatchToProps)(Main);
