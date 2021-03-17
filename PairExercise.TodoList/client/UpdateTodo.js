2e779facb01538ef33351cc8ef33de91fdd405c4
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-03-18 01:14:26 +0200

    update data on firestore functionality

--- PairExercise.TodoList/client/UpdateTodo.js
+++ PairExercise.TodoList/client/UpdateTodo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TodoForm from './TodoForm';
+import db from './firebase-config';
 
 export default class UpdateTodo extends Component {
   constructor(props) {
@@ -18,11 +19,16 @@ export default class UpdateTodo extends Component {
     });
   }
   async handleSubmit(e) {
+    /* comment in what is commented out if you want to use the local server api functionality */
+
     try {
       const { todo, updateTodo } = this.props;
       e.preventDefault();
-      const { data } = await axios.put(`/api/todos/${todo.id}`, this.state);
-      updateTodo(data);
+      // const { data } = await axios.put(`/api/todos/${todo.id}`, this.state);
+      await db.collection('todos').doc(todo.id).update(this.state); // no returned value from update method
+      const doc = await db.collection('todos').doc(todo.id).get();
+      const updatedDoc = { id: doc.id, ...doc.data() };
+      updateTodo(updatedDoc);
       this.setState({ taskName: '', assignee: '' });
     } catch (error) {
       console.log('there was an error in handleSubmit:', error);
