07638a3507a97807acd29e6e0df70ca3ed649e1c
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-03-21 18:04:17 +0200

    Login Route Created

--- PairExercise.Login/server/auth.js
+++ PairExercise.Login/server/auth.js
@@ -1,5 +1,27 @@
-const router = require('express').Router()
-const {User} = require('./db')
-module.exports = router
+const router = require('express').Router();
+const { User } = require('./db');
+module.exports = router;
 
 // auth routes go below!
+
+router.put('/login', async (req, res, next) => {
+  try {
+    const submittedUser = req.body;
+    const user = await User.findOne({
+      where: {
+        email: submittedUser.email,
+        password: submittedUser.password,
+      },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      // const err = new Error("Incorrect email or password!");
+      // err.status = 401;
+      // next(err);
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
