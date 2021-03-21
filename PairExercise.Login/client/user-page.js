04158521220aa76153bd3f925110721db4e9cb31
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-03-21 17:45:43 +0200

    Need user to see home page functionality

--- PairExercise.Login/client/user-page.js
+++ PairExercise.Login/client/user-page.js
@@ -1,39 +1,47 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 const UserPage = (props) => {
-  const {handleClick} = props
+  const { user, handleClick } = props;
 
-  return (
-    <div className='h100 w100 flex column align-items-center justify-center'>
-      <div className='flex'>
-        <img className='rounded mr1' />
-        <h1>Welcome back!</h1>
+  if (!user.id) {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <div className="h100 w100 flex column align-items-center justify-center">
+        <div className="flex">
+          <img className="rounded mr1" src={user.imgUrl} />
+          <h1>Welcome back {user.email}!</h1>
+        </div>
+        <div>
+          <button className="btn bg-red white p1 rounded" onClick={handleClick}>
+            Logout
+          </button>
+        </div>
       </div>
-      <div>
-        <button className='btn bg-red white p1 rounded' onClick={handleClick}>Logout</button>
-      </div>
-    </div>
-  )
-}
+    );
+  }
+};
 
 const mapStateToProps = (state, ownProps) => {
   return {
     // your code here
-  }
-}
+    user: state.user,
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   // Hey, check it out! Because we pass the connected UserPage to a Route
   // (we do this in client/index.js), it receives the "route props"
   // (match, location, and history) as its "own props".
-  const history = ownProps.history
+  const history = ownProps.history;
 
   return {
-    handleClick () {
+    handleClick() {
       // your code here
-    }
-  }
-}
+    },
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserPage)
+export default connect(mapStateToProps, mapDispatchToProps)(UserPage);
