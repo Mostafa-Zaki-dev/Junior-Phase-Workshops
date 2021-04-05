9e233be290d0c3ec56322d1ba49d257a47b27b93
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-04-05 18:14:59 +0200

    connect NewMessageEntry component

--- PairProject.StackChat/client/components/NewMessageEntry.js
+++ PairProject.StackChat/client/components/NewMessageEntry.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { writeMessage } from '../store';
 
-export default class NewMessageEntry extends Component {
-
-  render () {
+class NewMessageEntry extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.props.write(e.target.value);
+  }
+  render() {
     return (
       <form id="new-message-form">
         <div className="input-group input-group-lg">
@@ -11,12 +19,25 @@ export default class NewMessageEntry extends Component {
             type="text"
             name="content"
             placeholder="Say something nice..."
+            onChange={this.handleChange}
           />
           <span className="input-group-btn">
-            <button className="btn btn-default" type="submit">Chat!</button>
+            <button className="btn btn-default" type="submit">
+              Chat!
+            </button>
           </span>
         </div>
       </form>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  newMessageEntry: state.newMessageEntry,
+});
+
+mapDispatchToProps = (dispatch) => ({
+  write: (inputContent) => dispatch(writeMessage(inputContent)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewMessageEntry);
