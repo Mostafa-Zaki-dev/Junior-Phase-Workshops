06a17fb0cb470dec4c6c658303ba36ebb42797f3
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-04-05 15:35:13 +0200

    Moving MessageList to the store and connect it

--- PairProject.StackChat/client/components/MessagesList.js
+++ PairProject.StackChat/client/components/MessagesList.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
 import Message from './Message';
 import NewMessageEntry from './NewMessageEntry';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { fetchMessages } from '../store';
 
-export default class MessagesList extends Component {
-
-  constructor () {
-    super();
-    this.state = { messages: [] };
+class MessagesList extends Component {
+  componentDidMount() {
+    this.props.fetchInitialMessages();
   }
-
-  async componentDidMount () {
-    const response = await axios.get('/api/messages');
-    const messages = response.data;
-    this.setState({ messages });
-  }
-
-  render () {
-
+  render() {
     const channelId = Number(this.props.match.params.channelId); // because it's a string "1", not a number!
-    const messages = this.state.messages;
-    const filteredMessages = messages.filter(message => message.channelId === channelId);
+    const { messages } = this.props;
+    const filteredMessages = messages.filter((message) => message.channelId === channelId);
 
     return (
       <div>
         <ul className="media-list">
-          { filteredMessages.map(message => <Message message={message} key={message.id} />) }
+          {filteredMessages.map((message) => (
+            <Message message={message} key={message.id} />
+          ))}
         </ul>
         <NewMessageEntry />
       </div>
     );
   }
 }
+
+// using functional component:
+
+/* 
+const MessagesList = (props) => {
+  const channelId = +props.match.params.channelId; // because it's a string "1", not a number!
+  const { messages } = props;
+  const filteredMessages = messages.filter((message) => message.channelId === channelId);
+
+  return (
+    <div>
+      <ul className="media-list">
+        {filteredMessages.map((message) => (
+          <Message message={message} key={message.id} />
+        ))}
+      </ul>
+      <NewMessageEntry />
+    </div>
+  );
+}; 
+*/
+
+const mapStateToProps = (state) => ({
+  messages: state.messages,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchInitialMessages: () => dispatch(fetchMessages()),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(MessagesList);
