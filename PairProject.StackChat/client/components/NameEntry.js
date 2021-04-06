429c5fa53da0374a170d5f3a3fe5a1332f2d4e05
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-04-06 13:33:23 +0200

    Create NameEntry component

---
+++ PairProject.StackChat/client/components/NameEntry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NameEntry() {
+  return (
+    <form className="form-inline">
+      <label htmlFor="name">Your name:</label>
+      <input type="text" name="name" placeholder="Enter your name" className="form-control" />
+    </form>
+  );
+}
+
+export default NameEntry;
